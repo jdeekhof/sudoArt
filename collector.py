30c62375298843212a02984b3993e071edc97a66
Jacob Eekhof <jdeekhof@gmail.com>
2020-04-03 15:23:32 -0400

    wrote collector and now need to find a way to save

--- collector.py
+++ collector.py
@@ -1,5 +1,6 @@
 import praw
 import reddit_creds
+from PIL import Image
 
 reddit = praw.Reddit(client_id=reddit_creds.client_id,
                      client_secret=reddit_creds.client_secret,
@@ -7,6 +8,15 @@ reddit = praw.Reddit(client_id=reddit_creds.client_id,
                      user_agent=reddit_creds.user_agent,
                      username=reddit_creds.username)
 
-print(reddit.user.me())
+subreddit = reddit.subreddit('art').top('month')
+for item in subreddit:
+    item_dict = {'artist':item.author.name,
+        'original':item.is_self,
+        'title':item.title,
+         'link': item.url,
+          'score': item.score
+        }
+    if int(item_dict['score']) >= 10000:
+        Image.save(item_dict['link'])
 
 
